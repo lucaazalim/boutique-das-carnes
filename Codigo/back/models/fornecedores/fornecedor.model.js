999519e68e071a18b7d43cc5fff95ed704086775
Jhonata S. Dias <118060258+jhonstudentx@users.noreply.github.com>
2024-03-26 18:16:51 -0300

    #16 post e put fornecedores finalizado

--- Codigo/back/models/fornecedores/fornecedor.model.js
+++ Codigo/back/models/fornecedores/fornecedor.model.js
@@ -1,21 +1,84 @@
 const Fornecedor = require('../fornecedores/fornecedor.sequelize');
 
 async function getAllForcedores() {
-    return await Fornecedor.findAll({
-        attributes: { exclude: ['createdAt', 'updatedAt'] }
-    })
+    return await Fornecedor.findAll();
 }
 
 async function getByIdFornecedores(id) {
-    return await Fornecedor.findAll({
-        attributes: { exclude: ['createdAt', 'updatedAt'] },
-        where: {
-            id
-        }
+    return await Fornecedor.findByPk(id);
+}
+
+async function addFornecedores(
+    tipo,
+    email,
+    telefone,
+    celular,
+    cep,
+    logradouro,
+    bairro,
+    numero,
+    complemento,
+    estado,
+    cidade,
+    ativo,
+    notas) {
+        const data = await Fornecedor.create({
+        tipo,
+        email,
+        telefone,
+        celular,
+        cep,
+        logradouro,
+        bairro,
+        numero,
+        complemento,
+        estado,
+        cidade,
+        ativo,
+        notas
     })
+    return data;
+}
+
+async function updateFornecedores(
+    id,
+    tipo,
+    email,
+    telefone,
+    celular,
+    cep,
+    logradouro,
+    bairro,
+    numero,
+    complemento,
+    estado,
+    cidade,
+    ativo,
+    notas,
+) {
+    const data = await Fornecedor.update({
+        tipo,
+        email,
+        telefone,
+        celular,
+        cep,
+        logradouro,
+        bairro,
+        numero,
+        complemento,
+        estado,
+        cidade,
+        ativo,
+        notas,
+    }, {
+        where: { id }
+    })
+    return data;
 }
 
 module.exports = {
     getAllForcedores,
     getByIdFornecedores,
+    addFornecedores,
+    updateFornecedores,
 }

e838340c5d3632c97d8ea3f14800c98dd0d5fc7c
Fernando Antônio Ferreira Ibrahim <offut14@gmail.com>
2024-03-31 19:51:08 -0300

    alteracão padronizacao rotas #18

--- Codigo/back/routes/compras/compras.router.js
+++ Codigo/back/routes/compras/compras.router.js
@@ -26,13 +26,13 @@ const {
 const comprasRouter = express.Router();
 
 comprasRouter.get('/pesagens',httpGetAllComprasPesagens);
-comprasRouter.get('/pesagens/:id', httpGetComprasPesagensById);
+comprasRouter.get('/:idCompra/pesagens/:id', httpGetComprasPesagensById);
 comprasRouter.post('/pesagens', httpPostComprasPesagens);
 comprasRouter.post('/:idCompra/pesagens', httpPostManyCompraPesagens);
 comprasRouter.put('/:idCompra/pesagens/:id', httpPutCompraPesagensById);
 
 comprasRouter.get('/pagamentos',httpGetAllComprasPagamentos);
-comprasRouter.get('/pagamentos/:id', httpGetComprasPagamentosById);
+comprasRouter.get('/:idCompra/pagamentos/:id', httpGetComprasPagamentosById);
 comprasRouter.post('/pagamentos', httpPostComprasPagamentos);
 comprasRouter.post('/:idCompra/pagamentos', httpPostManyComprasPagamentos);
 comprasRouter.put('/:idCompra/pagamentos/:id', httpPutCompraPagamentoById);

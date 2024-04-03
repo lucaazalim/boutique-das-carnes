7cf1c82d710a1e2215c8cdf386395f4c6c1f6a3c
Luca Azalim <lucaazalim@gmail.com>
2024-04-02 21:29:08 -0300

    Corrigindo endereço do banco de dados no back.

--- Codigo/back/services/mariadb.service.js
+++ Codigo/back/services/mariadb.service.js
@@ -2,21 +2,21 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('boutique_das_carnes', 'root', 'password', {
     define: {
-        freezeTableName:true
+        freezeTableName: true
     },
-    host: 'localhost',
+    host: 'mariadb',
     dialect: 'mariadb',
-    port: 3307
+    port: 3306
 });
 
 async function connectToDatabase() {
     try {
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
-      } catch (error) {
+    } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
-    
+
 }
 
 module.exports = {

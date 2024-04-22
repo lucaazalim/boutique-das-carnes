d12e6d0d727cb362771698b05558aab85ece5b1a
RafaMtF <1452876@sga.pucminas.br>
2024-04-22 18:18:14 -0300

    Continuando a componentização do site e padronização do estilo

--- Codigo/front/app/components/ModalCriarFornecedor.js
+++ Codigo/front/app/components/ModalCriarFornecedor.js
@@ -82,180 +82,231 @@ function ModalCriarFornecedor({ isOpen, setIsOpen }) {
           ` fixed inset-0 bg-zinc-800/80 backdrop-blur bg-opacity-75 flex items-center justify-center z-50`
         }
       >
-        <div className="h-[calc(100vh-25%)] w-[calc(100vw-25%)] bg-white p-2 rounded-md">
-          <form onSubmit={handleSubmit}>
-            <div className="grid grid-cols-2 gap-2 pb-2">
-              <select
-                className="text-lg p-2 bg-gray-300 rounded-md"
-                name="tipo"
-                required
-                onChange={handleChange}
-              >
-                <option value="PF" defaultValue>
-                  Pessoa Física
+        <form
+          onSubmit={handleSubmit}
+          className="w-[calc(100vw-35%)] bg-white p-8 rounded-md grid grid-cols-2 gap-2"
+        >
+          <h1 className="text-4xl font-semibold mb-2 col-span-2">
+            Criar Funcionário
+          </h1>
+          <label className="bg-gray-300 p-2 rounded-md text-lg">
+            Tipo de pessoa:
+            <select
+              className="p-1 bg-gray-200 rounded-md w-full"
+              name="tipo"
+              required
+              onChange={handleChange}
+            >
+              <option value="PF" defaultValue>
+                Pessoa Física
+              </option>
+              <option value="PJ">Pessoa Jurídica</option>
+            </select>
+          </label>
+          {formData.tipo === "PF" ? (
+            <>
+              <label className="bg-gray-300 p-2 rounded-md text-lg">
+                CPF:
+                <input
+                  className="p-1 bg-gray-200 rounded-md w-full"
+                  required
+                  pattern="[0-9]{11}"
+                  name="cpf"
+                  value={formData.pessoa.cpf}
+                  onChange={handleChange}
+                  placeholder="CPF"
+                />
+              </label>
+              <label className="bg-gray-300 p-2 rounded-md text-lg">
+                Nome:
+                <input
+                  className="p-1 bg-gray-200 rounded-md w-full"
+                  required
+                  name="nome"
+                  value={formData.pessoa.nome}
+                  onChange={handleChange}
+                  placeholder="Nome"
+                />
+              </label>
+            </>
+          ) : (
+            <>
+              <label className="bg-gray-300 p-2 rounded-md text-lg">
+                CNPJ:
+                <input
+                  className="p-1 bg-gray-200 rounded-md w-full"
+                  required
+                  pattern="[0-9]{14}"
+                  name="cnpj"
+                  value={formData.pessoa.cnpj}
+                  onChange={handleChange}
+                  placeholder="CNPJ"
+                />
+              </label>
+              <label className="bg-gray-300 p-2 rounded-md text-lg">
+                Razão Social:
+                <input
+                  className="p-1 bg-gray-200 rounded-md w-full"
+                  required
+                  name="razao_social"
+                  value={formData.pessoa.razao_social}
+                  onChange={handleChange}
+                  placeholder="Razão Social"
+                />
+              </label>
+              <label className="bg-gray-300 p-2 rounded-md text-lg">
+                Nome Fantasia:
+                <input
+                  className="p-1 bg-gray-200 rounded-md w-full"
+                  name="nome_fantasia"
+                  value={formData.pessoa.nome_fantasia}
+                  onChange={handleChange}
+                  placeholder="Nome Fantasia"
+                />
+              </label>
+            </>
+          )}
+          <label className="bg-gray-300 p-2 rounded-md text-lg">
+            Email:
+            <input
+              className="p-1 bg-gray-200 rounded-md w-full"
+              type="email"
+              name="email"
+              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="E-mail"
+            />
+          </label>
+          <label className="bg-gray-300 p-2 rounded-md text-lg">
+            Telefone:
+            <input
+              className="p-1 bg-gray-200 rounded-md w-full"
+              name="telefone"
+              type="tel"
+              maxLength={11}
+              value={formData.telefone}
+              onChange={handleChange}
+              placeholder="Telefone"
+            />
+          </label>
+          <label className="bg-gray-300 p-2 rounded-md text-lg">
+            Celular:
+            <input
+              className="p-1 bg-gray-200 rounded-md w-full"
+              name="celular"
+              value={formData.celular}
+              onChange={handleChange}
+              placeholder="Celular"
+            />
+          </label>
+          <label className="bg-gray-300 p-2 rounded-md text-lg">
+            CEP:
+            <input
+              className="p-1 bg-gray-200 rounded-md w-full"
+              required
+              name="cep"
+              value={formData.cep}
+              onChange={handleChange}
+              placeholder="CEP"
+            />
+          </label>
+          <label className="bg-gray-300 p-2 rounded-md text-lg">
+            Logradouro:
+            <input
+              className="p-1 bg-gray-200 rounded-md w-full"
+              required
+              name="logradouro"
+              value={formData.logradouro}
+              onChange={handleChange}
+              placeholder="Logradouro"
+            />
+          </label>
+          <label className="bg-gray-300 p-2 rounded-md text-lg">
+            Bairro:
+            <input
+              className="p-1 bg-gray-200 rounded-md w-full"
+              required
+              name="bairro"
+              value={formData.bairro}
+              onChange={handleChange}
+              placeholder="Bairro"
+            />
+          </label>
+          <label className="bg-gray-300 p-2 rounded-md text-lg">
+            Número:
+            <input
+              className="p-1 bg-gray-200 rounded-md w-full"
+              name="numero"
+              value={formData.numero}
+              onChange={handleChange}
+              placeholder="Número"
+            />
+          </label>
+          <label className="bg-gray-300 p-2 rounded-md text-lg">
+            Complemento:
+            <input
+              className="p-1 bg-gray-200 rounded-md w-full"
+              name="complemento"
+              value={formData.complemento}
+              onChange={handleChange}
+              placeholder="Complemento"
+            />
+          </label>
+          <label className="bg-gray-300 p-2 rounded-md text-lg">
+            Estado:
+            <select
+              className="p-1 bg-gray-200 rounded-md w-full"
+              name="estado"
+              onChange={(e) => handleChange(e)}
+              required
+            >
+              {estados.map((estado) => (
+                <option key={estado.sigla} value={estado.sigla}>
+                  {estado.nome}
                 </option>
-                <option value="PJ">Pessoa Jurídica</option>
-              </select>
-              {formData.tipo === "PF" ? (
-                <>
-                  <input
-                    className="text-lg p-2 bg-gray-300 rounded-md"
-                    required
-                    pattern="[0-9]{11}"
-                    name="cpf"
-                    value={formData.pessoa.cpf}
-                    onChange={handleChange}
-                    placeholder="CPF"
-                  />
-                  <input
-                    className="text-lg p-2 bg-gray-300 rounded-md"
-                    required
-                    name="nome"
-                    value={formData.pessoa.nome}
-                    onChange={handleChange}
-                    placeholder="Nome"
-                  />
-                </>
-              ) : (
-                <>
-                  <input
-                    className="text-lg p-2 bg-gray-300 rounded-md"
-                    required
-                    pattern="[0-9]{14}"
-                    name="cnpj"
-                    value={formData.pessoa.cnpj}
-                    onChange={handleChange}
-                    placeholder="CNPJ"
-                  />
-                  <input
-                    className="text-lg p-2 bg-gray-300 rounded-md"
-                    required
-                    name="razao_social"
-                    value={formData.pessoa.razao_social}
-                    onChange={handleChange}
-                    placeholder="Razão Social"
-                  />
-                  <input
-                    className="text-lg p-2 bg-gray-300 rounded-md"
-                    name="nome_fantasia"
-                    value={formData.pessoa.nome_fantasia}
-                    onChange={handleChange}
-                    placeholder="Nome Fantasia"
-                  />
-                </>
-              )}
-              <input
-                className="text-lg p-2 bg-gray-300 rounded-md"
-                type="email"
-                name="email"
-                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}"
-                value={formData.email}
-                onChange={handleChange}
-                placeholder="E-mail"
-              />
-              <input
-                className="text-lg p-2 bg-gray-300 rounded-md"
-                name="telefone"
-                type="tel"
-                maxLength={11}
-                value={formData.telefone}
-                onChange={handleChange}
-                placeholder="Telefone"
-              />
-              <input
-                className="text-lg p-2 bg-gray-300 rounded-md"
-                name="celular"
-                value={formData.celular}
-                onChange={handleChange}
-                placeholder="Celular"
-              />
-              <input
-                className="text-lg p-2 bg-gray-300 rounded-md"
-                required
-                name="cep"
-                value={formData.cep}
-                onChange={handleChange}
-                placeholder="CEP"
-              />
-              <input
-                className="text-lg p-2 bg-gray-300 rounded-md"
-                required
-                name="logradouro"
-                value={formData.logradouro}
-                onChange={handleChange}
-                placeholder="Logradouro"
-              />
-              <input
-                className="text-lg p-2 bg-gray-300 rounded-md"
-                required
-                name="bairro"
-                value={formData.bairro}
-                onChange={handleChange}
-                placeholder="Bairro"
-              />
-              <input
-                className="text-lg p-2 bg-gray-300 rounded-md"
-                name="numero"
-                value={formData.numero}
-                onChange={handleChange}
-                placeholder="Número"
-              />
-              <input
-                className="text-lg p-2 bg-gray-300 rounded-md"
-                name="complemento"
-                value={formData.complemento}
-                onChange={handleChange}
-                placeholder="Complemento"
-              />
-              <select
-                className="text-lg p-2 bg-gray-300 rounded-md"
-                name="estado"
-                value={formData.estado}
-                onChange={(e) => handleChange(e)}
-                required
-              >
-                {estados.map((estado) => (
-                  <option key={estado.sigla} value={estado.sigla}>
-                    {" "}
-                    {estado.nome}
-                  </option>
-                ))}
-              </select>
-              <input
-                className="text-lg p-2 bg-gray-300 rounded-md"
-                required
-                name="cidade"
-                value={formData.cidade}
-                onChange={handleChange}
-                placeholder="Cidade"
-              />
-              <textarea
-                className="text-lg p-2 bg-gray-300 rounded-md col-span-2"
-                name="notas"
-                value={formData.notas}
-                onChange={handleChange}
-                placeholder="Notas"
-              />
-            </div>
-            <div className="flex justify-between mt-[210px]">
-              <button
-                className="bg-[#9e1616] p-2 rounded-md text-white font-semibold"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setIsOpen(!isOpen);
-                }}
-              >
-                Cancelar
-              </button>
-              <button
-                className="bg-[#06BD18] p-2 rounded-md text-white font-semibold"
-                type="submit"
-              >
-                Criar
-              </button>
-            </div>
-          </form>
-        </div>
+              ))}
+            </select>
+          </label>
+          <label className="bg-gray-300 p-2 rounded-md text-lg">
+            Cidade:
+            <input
+              className="p-1 bg-gray-200 rounded-md w-full"
+              required
+              name="cidade"
+              value={formData.cidade}
+              onChange={handleChange}
+              placeholder="Cidade"
+            />
+          </label>
+          <label className="bg-gray-300 p-2 rounded-md text-lg col-span-2">
+            Notas:
+            <textarea
+              className="p-1 bg-gray-200 rounded-md w-full"
+              name="notas"
+              value={formData.notas}
+              onChange={handleChange}
+              placeholder="Notas"
+            />
+          </label>
+          <div className="flex justify-between col-span-2">
+            <button
+              className="bg-red-500 text-white p-2 rounded-md"
+              onClick={(e) => {
+                e.preventDefault();
+                setIsOpen(!isOpen);
+              }}
+            >
+              Cancelar
+            </button>
+            <button
+              className="bg-green-500 text-white p-2 rounded-md"
+              type="submit"
+            >
+              Criar
+            </button>
+          </div>
+        </form>
       </div>
     </>
   );
--- Codigo/front/app/compras/page.js
+++ Codigo/front/app/compras/page.js
@@ -4,27 +4,9 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import Table from "../components/Table";
+import ModalCriarCompra from "../components/ModalCriarCompra";
 
 export default function Home() {
-  const [formData, setFormData] = useState({
-    id_fornecedor: "",
-    status: "",
-    unidades_macho: "",
-    unidades_femea: "",
-    preco_arroba: "",
-    desconto: "",
-    animais_abatidos: "",
-    peso_total_abate: "",
-    id_documento_romaneio: null,
-    id_documento_gta: null,
-    id_documento_nf_compra: null,
-    id_documento_nf_abate: null,
-    id_documento_nfs_matadouro: null,
-    id_documento_nf_retorno: null,
-    pagamentos: {},
-    pesagens: {},
-  });
-
   const [compras, setCompras] = useState([]);
   const [open, setOpen] = useState(false);
   const [pesquisa, setPesquisa] = useState("");
@@ -45,34 +27,7 @@ export default function Home() {
         setCompras(data);
       })
       .catch((e) => console.error("Erro ao solicitar os dados: " + e));
-  }, [pesquisa]);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const res = await fetch("http://localhost:3001/compras", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-        redirect: "follow",
-      });
-      alert("Compra inserida com sucesso.");
-      setOpen(!open);
-      router.push("/compras");
-    } catch (error) {
-      console.error("Erro ao inserir dado no banco.");
-    }
-  };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      [name]: value,
-    }));
-  };
+  }, []);
 
   return (
     <>
@@ -94,92 +49,9 @@ export default function Home() {
         </div>
         <Table headers={headers} data={{ compras }} />
       </div>
-      <div
-        className={
-          (open ? `` : `hidden`) +
-          ` fixed inset-0 bg-gray-500 bg-opacity-75 flex items-center justify-center z-50`
-        }
-      >
-        <div className="h-[calc(100vh-25%)] w-[calc(100vw-25%)] bg-white p-2 rounded-md">
-          <form onSubmit={handleSubmit}>
-            <div className="grid grid-cols-2 gap-2 pb-2">
-              <input
-                className="text-lg p-2 bg-gray-300 rounded-md"
-                name="id_fornecedor"
-                value={formData.id_fornecedor}
-                onChange={handleChange}
-                placeholder="Id fornecedor"
-              />
-              <input
-                className="text-lg p-2 bg-gray-300 rounded-md"
-                name="status"
-                value={formData.status}
-                onChange={handleChange}
-                placeholder="Status"
-              />
-              <input
-                className="text-lg p-2 bg-gray-300 rounded-md"
-                name="unidades_macho"
-                value={formData.unidades_macho}
-                onChange={handleChange}
-                placeholder="Unidades macho"
-              />
-              <input
-                className="text-lg p-2 bg-gray-300 rounded-md"
-                name="unidades_femea"
-                value={formData.unidades_femea}
-                onChange={handleChange}
-                placeholder="Unidades fêmea"
-              />
-              <input
-                className="text-lg p-2 bg-gray-300 rounded-md"
-                name="preco_arroba"
-                value={formData.preco_arroba}
-                onChange={handleChange}
-                placeholder="Preço da arroba"
-              />
-              <input
-                className="text-lg p-2 bg-gray-300 rounded-md"
-                name="desconto"
-                value={formData.desconto}
-                onChange={handleChange}
-                placeholder="Desconto"
-              />
-              <input
-                className="text-lg p-2 bg-gray-300 rounded-md"
-                name="animais_abatidos"
-                value={formData.animais_abatidos}
-                onChange={handleChange}
-                placeholder="Animais abatidos"
-              />
-              <input
-                className="text-lg p-2 bg-gray-300 rounded-md"
-                name="peso_total_abate"
-                value={formData.peso_total_abate}
-                onChange={handleChange}
-                placeholder="Peso total do abate"
-              />
-            </div>
-            <div className="flex justify-between">
-              <button
-                className="bg-[#9e1616] p-2 rounded-md text-white font-semibold"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setOpen(!open);
-                }}
-              >
-                Cancelar
-              </button>
-              <button
-                className="bg-[#06BD18] p-2 rounded-md text-white font-semibold"
-                type="submit"
-              >
-                Enviar
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
+
+      <ModalCriarCompra open={open} setOpen={setOpen} />
+
       <Link
         href="/dashboard"
         className="flex justify-center w-[80px] p-2 bg-slate-400 rounded-md absolute bottom-5 left-[140px] text-white"

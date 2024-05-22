b864ec5b3a02af2889d2a25497a1a18e6ebfee29
RafaMtF <1452876@sga.pucminas.br>
2024-05-21 23:57:11 -0300

    Edições na tabela de estoque

--- Codigo/front/app/estoque/TableEstoque.jsx
+++ Codigo/front/app/estoque/TableEstoque.jsx
@@ -1,25 +1,38 @@
+import Link from "next/link";
 import React from "react";
 
 function TableResumo({ estoque }) {
   return (
-    <table className="w-full overflow-auto">
+    <table className="w-full">
       <thead>
-        <tr className="grid grid-cols-4">
-          <th className="p-2 border-r-2 border-gray-300">ID</th>
-          <th className="p-2 border-r-2 border-gray-300">ID da Compra</th>
+        <tr className="grid grid-cols-3">
+          {/* <th className="p-2 border-r-2 border-gray-300">ID</th>
+          <th className="p-2 border-r-2 border-gray-300">ID da Compra</th> */}
           <th className="p-2 border-r-2 border-gray-300">Letra</th>
-          <th className="p-2">Tipo</th>
+          <th className="p-2 border-r-2 border-gray-300">Tipo</th>
+          <th className="p-2">Link para compra</th>
         </tr>
       </thead>
       <tbody className="border-t-2 border-gray-300">
-        {estoque && estoque.map(item => (
-          <tr key={item.id} className="grid grid-cols-4">
-            <td className="p-2 border-r-2 border-gray-300 flex items-center justify-center">{item.id}</td>
-            <td className="p-2 border-r-2 border-gray-300 flex items-center justify-center">{item.id_compra_carcaca}</td>
-            <td className="p-2 border-r-2 border-gray-300 flex items-center justify-center">{item.id_compra_carcaca}</td>
-            <td className="p-2 flex items-center justify-center">{item.tipo}</td>
-          </tr>
-        ))}
+        {estoque &&
+          estoque.map((item) => (
+            <tr key={item.id} className="grid grid-cols-3">
+              <td className="p-2 border-r-2 border-gray-300 flex items-center justify-center">
+                {item.id_compra_carcaca}
+              </td>
+              <td className="p-2 border-r-2 border-gray-300 flex items-center justify-center">
+                {item.tipo}
+              </td>
+              <td className="p-2 flex items-center justify-center">
+                <Link
+                  className="text-blue-500"
+                  href={`/compras/${item.id_compra_carcaca}`}
+                >
+                  Link
+                </Link>
+              </td>
+            </tr>
+          ))}
       </tbody>
     </table>
   );

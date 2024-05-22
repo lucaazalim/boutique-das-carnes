0d3d193afdfda0f658436e77f16490a6ebf30f4c
Jhonata S. Dias <118060258+jhonstudentx@users.noreply.github.com>
2024-05-22 07:18:34 -0300

    #23 Correções nas rotas itens do pedido

--- Codigo/back/routes/pedidos/pedido.router.js
+++ Codigo/back/routes/pedidos/pedido.router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const pedidoRouter = express.Router();
+
 const {
     httpGetAllPedido,
     httpGetByIdPedido,
@@ -7,7 +8,17 @@ const {
     httpPutPedido,
     httpDeletePedido,
 } = require('./pedido.controller');
+const {
+    httpGetByIdPedidoItem,
+    httpGetByIdItem,
+    httpPostItem,
+    httpDeleteItem,
+} = require('./item/item.controller');
 
+pedidoRouter.get('/:id_pedido/itens', httpGetByIdPedidoItem);
+pedidoRouter.get('/itens/:id', httpGetByIdItem);
+pedidoRouter.post('/itens', httpPostItem);
+pedidoRouter.delete('/itens/:id', httpDeleteItem);
 pedidoRouter.get('/', httpGetAllPedido);
 pedidoRouter.get('/:id', httpGetByIdPedido);
 pedidoRouter.post('/', httpPostPedido);
